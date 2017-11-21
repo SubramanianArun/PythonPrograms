51f7834777380e6bda4420a3c7e7c79a0f7d5e23
Vijayadeepak <subramanian.a@outlook.com>
2017-11-21 00:56:34 -0600

    Adding unit test functions

--- ShortestPalindromicBase.py
+++ ShortestPalindromicBase.py
@@ -2,12 +2,23 @@
 import sys
 import time
 
-#Import Modules
-import sys
-import time
-
+#Define input validation function
+def validate_input (number) :
+    if number is None:
+        return None
+    elif type(number) == type(0):
+        number = [int(convert) for convert in str(number)] #If it is an integer, convert it into an integer array
+        return number
+    elif type(number) == type({}):
+        if number[0] == type(int): #Check if Hashmap values have integer key values or else discard input
+            return number
+        else :
+            return None
+    else :                   #Discard all other data type inputs
+        return None
 ##Defining palindrome checking function
 def palindrome (number):
+  number = validate_input(number)
   endpointer = len(number)-1
   for startpointer in range(0,len(number)):
     if (number[startpointer] != number[endpointer]):
@@ -19,58 +30,19 @@ def palindrome (number):
 def base (value , n):
   new_number = {}
   iterator = 0
-  while (value != 0):
-    remainder = str(value % n)
-    value = int(value/n)
-    new_number[iterator] = remainder
-    iterator += 1
-  return new_number
+  if n <= 1 or value <= 0 or isinstance(value, float) or isinstance(n , float):
+    return None
+  else:
+    while (value != 0):
+        remainder = int(value % n)
+        value = int(value/n)
+        new_number[iterator] = remainder
+        iterator += 1
+    return new_number
 
 
 
-#start = time.clock()       #Timer to measure the run time
-
-#Initialize a dictionary
-hash_map = {1:None}
-n = 1000 #Enter the number upto which the palindromic base needs to be found
-for iterator in range(1,n+1):
-  hash_map[iterator] = None
-
-#Build the dictionary
-for value_iterator in range(1,n+1):
-  for base_iterator in range(2,int(value_iterator/2)+1):     #Check until the half of the given number
-    if(palindrome(base(value_iterator,base_iterator)) and hash_map[value_iterator] == None):
-      hash_map[value_iterator] = base_iterator
-  if(hash_map[value_iterator] == None):         #If not found, check for n-1 value
-    hash_map[value_iterator] = palindrome(base(value_iterator,value_iterator-1))
-#Brute Force Method
-  '''for i in range(2,n):
-    for j in range(1,n+1):
-      if(palindrome(base(j,i)) and hash_map[j] == None):
-        hash_map[j] = i'''
-
-#Printing the hash_map
-print ("decimal", "smallest base in which the number is a palindrome")
-for value in hash_map:
-  print(value, hash_map[value])
-
-#Snippet to print the run time
-'''elapsed = time.clock() - start
-print('%.4f seconds' % elapsed)'''
-
-
-#Finding the maximum base value for the palindrome
-'''maximum = 0
-for value_iterator in hash_map:
-  if(hash_map[value_iterator] <= 40):
-    counter+= 1
-  if(hash_map[value_iterator] >= maximum):
-    maximum = hash_map[value_iterator]'''
-
-
-
-
-#start = time.clock()       #Timer to measure the run time
+start = time.clock()       #Timer to measure the run time
 
 #Initialize a dictionary
 hash_map = {1:None}
@@ -79,22 +51,28 @@ for iterator in range(1,n+1):
   hash_map[iterator] = None
 
 #Build the dictionary
+inner_loop_iterator = 0
 for value_iterator in range(1,n+1):
-  for base_iterator in range(2,int(value_iterator)+1):     #Check until the half of the given number
+  if (value_iterator < 10): #Handling edge cases for numbers less than 10
+      inner_loop_iterator = 10
+  else:
+      inner_loop_iterator = int(value_iterator/2)
+  for base_iterator in range(2,inner_loop_iterator+1):     #Check until the half of the given number
     if(palindrome(base(value_iterator,base_iterator)) and hash_map[value_iterator] == None):
       hash_map[value_iterator] = base_iterator
-  if(hash_map[value_iterator] == None):         #If not found, check for n-1 value
-    hash_map[value_iterator] = palindrome(base(value_iterator,n-1))
+  if(hash_map[value_iterator] == None):   #If not found, assign n-1 value
+    hash_map[value_iterator] = value_iterator-1
+
 #Brute Force Method
   '''for i in range(2,n):
-    for j in range(1,n+1):
-      if(palindrome(base(j,i)) and hash_map[j] == None):
-        hash_map[j] = i'''
+        for j in range(1,n+1):
+            if(palindrome(base(j,i)) and hash_map[j] == None):
+                hash_map[j] = i'''
 
 #Printing the hash_map
-print ("decimal", "smallest base in which the number is a palindrome")
+print "decimal", "smallest base in which the number is a palindrome"
 for value in hash_map:
-  print(value, hash_map[value])
+  print value, hash_map[value]
 
 #Snippet to print the run time
 '''elapsed = time.clock() - start
---
+++ test_palindome.py
@@ -0,0 +1,28 @@
+import unittest
+import ShortestPalindromicBase
+
+class TestShortestPalindome(unittest.TestCase):
+
+    def test_base(self):
+        self.assertEqual(ShortestPalindromicBase.base(26,5),{0:1,1:0,2:1})
+        self.assertEqual(ShortestPalindromicBase.base(19,18),{0:1,1:1})
+        self.assertEqual(ShortestPalindromicBase.base(49,7),{0:0,1:0,2:1})
+        self.assertEqual(ShortestPalindromicBase.base(42,33),{0:9,1:1})
+        self.assertEqual(ShortestPalindromicBase.base(26,0), None)
+        self.assertEqual(ShortestPalindromicBase.base(31,1), None)
+        self.assertEqual(ShortestPalindromicBase.base(-45,1), None)
+        self.assertEqual(ShortestPalindromicBase.base(-2, -11), None)
+        self.assertEqual(ShortestPalindromicBase.base(4.5, 3), None)
+        self.assertEqual(ShortestPalindromicBase.base(5, 3.4), None)
+
+    def test_palindrome(self):
+        self.assertEqual(ShortestPalindromicBase.palindrome({0:'1',1:'0',2:'1'}), None)
+        self.assertEqual(ShortestPalindromicBase.palindrome('101'), None)
+        self.assertEqual(ShortestPalindromicBase.palindrome('abba'), None)
+        self.assertEqual(ShortestPalindromicBase.palindrome(1221), True)
+        self.assertEqual(ShortestPalindromicBase.palindrome(None), None)
+        self.assertEqual(ShortestPalindromicBase.palindrome(False), None)
+        #self.assertEqual(ShortestPalindromicBase.palindrome(14.41), None)
+
+if __name__ == '__main__' :
+    unittest.main()
